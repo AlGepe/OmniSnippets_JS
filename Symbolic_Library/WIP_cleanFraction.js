eca1dcf25586d2e2e28c43833702fe4a68cc0d42
Al Gepe <alvarodiezgp@gmail.com>
2019-11-08 12:26:40 +0100

    should be all useable functions commented

--- Symbolic_Library/WIP_cleanFraction.js
+++ Symbolic_Library/WIP_cleanFraction.js
@@ -1,27 +1,32 @@
-// Simplifies fraction (Accepts strings with square roots)
+/*
+    * NOT TESTED
+    * Simplifies a fraction (accepts square roots in numerator/denominator)
+    * INPUT: numbers (1:numberator, 2:denominator)
+    * OUTPUT: array of 2 numbers ([simplifiedNumerator, simplifiedDenominator])
+    * REQUIREST: reduceFraction
+*/
 function cleanFraction(upper, lower){
   var upNumbs = upper*upper,
       lowNumbs = lower*lower,
       fraction = [],
       a,
       b;
-      //return [typeof upper, "test"];
   if(typeof upper != "number"){
     upNumbs = 1;
     a = parseInt(upper.split("√")[0]);
     b = parseInt(upper.split("√")[1]);
     if(!(b.isNaN)){upNumbs = b;}
-    if(-1*(a.isNaN)){upNumbs = upNumbs*a*a;}
+    if(!(isNaN(a))){upNumbs = upNumbs*a*a;}
   }
   if(typeof lower != "number"){
     lowNumbs = 1;
     a = parseInt(lower.split("√")[0]);
     b = parseInt(lower.split("√")[1]);
     if(!(b.isNaN)){lowNumbs = b;}
-    if(-1*(a.isNaN)){lowNumbs = lowNumbs*a*a;}
-    //return [a, "juan"];
-   // return [upper,lowNumbs];
+    if(!(isNaN(a))){lowNumbs = lowNumbs*a*a;}
+
   }
-  fraction = reduceFraction(upNumbs, lowNumbs);
+  fraction = reduce(upNumbs, lowNumbs);
   return [findRoot(fraction[0]), findRoot(fraction[1])];
 }
+
