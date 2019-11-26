c04026c05c7057f3473abf9f84d1cb185a3b31fb
Al Gepe <alvarodiezgp@gmail.com>
2019-11-26 11:05:32 +0100

    newVsetter

---
+++ Code/valueSetters/canSize.js
@@ -0,0 +1,9 @@
+var canSizeData = [{"name":"UK Standard 250ml",  "uid":"60", "values":{"can_height":"9.2",    "can_width":"6.63"}},
+                   {"name":"UK Standard 330ml",  "uid":"61", "values":{"can_height":"11.52",  "can_width":"6.63"}},
+                   {"name":"UK Standard 440ml",  "uid":"62", "values":{"can_height":"14.99",  "can_width":"6.62"}},
+                   {"name":"UK Standard 500ml",  "uid":"63", "values":{"can_height":"16.8",   "can_width":"6.62"}},
+                   {"name":"US Standard 8 oz.",  "uid":"64", "values":{"can_height":"6.915",  "can_width":"6.6"}},
+                   {"name":"US Standard 12 oz.", "uid":"65", "values":{"can_height":"10.373", "can_width":"6.6"}},
+                   {"name":"US Standard 16 oz.", "uid":"66", "values":{"can_height":"13.83",  "can_width":"6.6"}},
+                   {"name":"Custom", "uid":"70","values":{}}
+                  ];
