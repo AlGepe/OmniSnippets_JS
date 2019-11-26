97648d6a01ae06620b738392562f113c19eb3f5b
Al Gepe <alvarodiezgp@gmail.com>
2019-11-26 10:55:22 +0100

    fixed get Name

--- Code/Miscelaneous/getNameFromVSetter.js
+++ Code/Miscelaneous/getNameFromVSetter.js
@@ -1,27 +1,27 @@
 /*
-    * Returns the name of a an item in a value setter give its UID
-    * workd for Value Selects with value instead of uid
-    *
-    * INPUT: - uid as a number
-    *        - variable storing the value setter (array of dictionaries)
-    * OUTPUT: Displayed name of the option (string) or error message
-*/
+  * Returns the name of a an item in a value setter give its UID
+  * works for Value Selects with value instead of uid
+  *
+  * INPUT: - uid (vSetter) or value (vSelect) as a number
+  *        - variable storing the value setter (array of dictionaries)
+  * OUTPUT: Displayed name of the option (string) or error message
+  */
 function getNameFromVSetter(thisUid, vSetter){
-    var name;
-    if(vSetter.uid === undefined){ //it's a Value Select
-        name = "There is no element with VALUE: "+thisUid+" in your Value Select";
-        for(var i = 0; i < vSetter.length;i++){
-            if(vSetter[i].value == thisUid){
-                return vSetter[i].name;
-            }
-        }
-    }else{
-        name = "There is no element with UID: "+thisUid+" in your Value Setter";
-        for(var i = 0; i < vSetter.length;i++){
-            if(vSetter[i].uid == thisUid){
-                return vSetter[i].name;
-            }
-        }
+  var outPut;
+  if(!vSetter[0]){ //it's a Value Select
+    outPut = "There is no element with VALUE: "+thisUid+" in your Value Select";
+    for(var i = 0; i < vSetter.items.length; i++){
+      if(vSetter.items[i].value == thisUid){
+        return vSetter.items[i].name;
+      }
     }
-    return name;
+  }else{
+    outPut = "There is no element with UID: "+thisUid+" in your Value Setter";
+    for(var i = 0; i < vSetter.length;i++){
+      if(vSetter[i].uid == thisUid){
+        return vSetter[i].name;
+      }
+    }
+  }
+  return outPut;
 }
